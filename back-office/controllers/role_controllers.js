b5d5e24c8eb23e0a9234fd411a89d1dd692c4c4d
jindessi <96311944+Nomenandrianina@users.noreply.github.com>
2023-01-11 09:55:32 +0300

    Create Role controllers
    Create Role Controllers.js

---
+++ back-office/controllers/role_controllers.js
@@ -0,0 +1,18 @@
+require("dotenv").config(); // load .env variables
+const { Router } = require("express"); // import router from express
+const Role = require("../models/role"); 
+
+const router = Router();
+
+router.post("/create_role", async (req, res) => {
+    try {
+      // create a new role
+      const user = await Role.create(req.body);
+      // send new user as response
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  });
+
+  module.exports = router;
