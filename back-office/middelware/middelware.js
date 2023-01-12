174dbee39aaab05d8121f9f9fb500161335366e0
harilovajohnny <82762702+harilovajohnny@users.noreply.github.com>
2023-01-12 14:41:47 +0300

    generate email for client
    generate email for client

--- back-office/middelware/middelware.js
+++ back-office/middelware/middelware.js
@@ -1,5 +1,36 @@
 require("dotenv").config(); // loading env variables
+const template=require("../mail/mail_template")
 const jwt = require("jsonwebtoken");
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "harilovajohnny@gmail.com",
+    pass: "fywyivpjwawkspkj",
+  },
+});
+
+
+const mail_user= async(req, res)=>{
+  console.log("request",req.body.email);
+  const message = "nouvelle inscription"
+  const options = {
+    from: "harilovajohnny@gmail.com", // sender address
+    to: req.body.email, // receiver email
+    subject: "Inscription d'un nouveau client", // Subject line
+    text: "Votre inscription à notre site  est bien réussie",
+    html:template(message)
+  }
+
+  try {
+    const info = await transporter.sendMail(options)
+  }catch (error) {
+    console.log(error);
+  } 
+}
 
 // MIDDLEWARE FOR AUTHORIZATION (MAKING SURE THEY ARE LOGGED IN)
 const isLoggedIn = async (req, res, next) => {
@@ -28,7 +59,11 @@ const isLoggedIn = async (req, res, next) => {
   }
 };
 
+
+
+
+
 // export custom middleware
 module.exports = {
-  isLoggedIn,
+  isLoggedIn,mail_user
 };
