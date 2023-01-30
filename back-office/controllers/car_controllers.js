2d79ab244df2d531056481799aa7746e654bf839
jindessi <96311944+Nomenandrianina@users.noreply.github.com>
2023-01-30 12:51:12 +0300

    Update bon de sortie
    Update bon de sortie

--- back-office/controllers/car_controllers.js
+++ back-office/controllers/car_controllers.js
@@ -88,8 +88,7 @@ router.post("/create_car", async (req, res) => {
   router.post("/client/bon-sortie", async (req, res) =>{
     try {
       const car = await Car.findById(req.body.id);
-      console.log(car);
-      const reparation = await Reparation.find({Car: car._id}).populate("Car");
+      const reparation = await Reparation.find({Car: car._id}).populate(["Car","Piece"]);
       res.status(200).json({ status:200,reparation,car });
     } catch (error) {
       res.status(400).json({ error });
